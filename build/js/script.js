7d4158bbea6cec0515792d1dd1f3632a9958d5fd
nanawebdev <nanaprettysinger@gmail.com>
2021-11-10 17:19:24 +0300

    draft

--- build/js/script.js
+++ build/js/script.js
@@ -9,9 +9,10 @@ var popup = document.querySelector('.popup-wrapper');
 var phoneInput = document.querySelector('.js-phone-input');
 var openPopupButton = document.querySelector('.js-open-popup');
 var closePopupButton = document.querySelector('.js-close-popup');
-var overlay = document.querySelector('.overlay');
 var nameInput = document.querySelector('.js-name-input');
-var form = document.querySelector('form');
+var form = document.querySelector('.js-popup-form');
+var overlay = document.querySelector('.overlay');
+var messageInput = document.querySelector('.js-message-input');
 
 function menuTogglerOffice() {
   office.classList.toggle('js-menu-open');
@@ -26,37 +27,76 @@ function menuTogglerPages() {
 function openPopup() {
   popup.classList.add('js-popup--open');
   nameInput.focus();
-  overlay.style.display = 'block';
   var previousBodyWidth = document.body.clientWidth;
   document.body.style.overflow = 'hidden';
   var scrollWidth = document.body.clientWidth - previousBodyWidth;
   document.body.style.marginRight = document.body.clientWidth + scrollWidth;
+
+  document.onkeydown = function(e) {
+    if (e.key === 'Escape') {
+      closePopup();
+    }
+  };
 }
 
 function closePopup() {
   popup.classList.remove('js-popup--open');
-  overlay.style.display = 'none';
   document.body.style.overflow = 'scroll';
 }
 
-function validation() {
+// function validation(value) {
 
-}
+// }
 
-function setMask() {
-  phoneInput.setAttribute('value', '+7');
-}
+// function setMask(value) {
+
+//   function createMask(value) {
+//     var matrix = '+7 (___)_______';
+//     var i = 0;
+
+//     // возьми все не цифры и замени пустым местом. вернется 7
+//     var def = matrix.replace(/\D/g, '');
+//     var val = value.replace(/\D/g, '');
+
+//     if (def.length >= val.length) {
+//       val = def;
+//     }
+
+//     value = matrix.replace(/./g, function (a) {
+//       if (/[_\d]/.test(a) && i < val.length) {
+//         val.charAt(i++);
+
+//         if (i >= val.length) {
+//           return '';
+//         }
+//         return a;
+//       }
+//       // return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? '' : a;
+//     });
+//   }
+
+// }
 
 function onSubmitForm(e) {
   e.preventDefault();
+  var userPhone = phoneInput.value;
+  var userName = nameInput.value;
+  var userMessage = messageInput.value;
+  // validation(userPhone);
+
+  localStorage.setItem('phone', userPhone);
+  localStorage.setItem('name', userName);
+  localStorage.setItem('message', userMessage);
+
 }
 
+form.addEventListener('submit', onSubmitForm);
 openPopupButton.addEventListener('click', openPopup);
 closePopupButton.addEventListener('click', closePopup);
 pages.addEventListener('click', menuTogglerPages);
 office.addEventListener('click', menuTogglerOffice);
-phoneInput.addEventListener('change', validation);
-phoneInput.addEventListener('click', setMask);
+// phoneInput.addEventListener('change', setMask(phoneInput.target.value));
+// phoneInput.addEventListener('click', setMask);
 overlay.addEventListener('click', closePopup);
-form.addEventListener('submit', onSubmitForm);
+
 
--- source/js/script.js
+++ source/js/script.js
@@ -9,9 +9,10 @@ var popup = document.querySelector('.popup-wrapper');
 var phoneInput = document.querySelector('.js-phone-input');
 var openPopupButton = document.querySelector('.js-open-popup');
 var closePopupButton = document.querySelector('.js-close-popup');
-var overlay = document.querySelector('.overlay');
 var nameInput = document.querySelector('.js-name-input');
-var form = document.querySelector('form');
+var form = document.querySelector('.js-popup-form');
+var overlay = document.querySelector('.overlay');
+var messageInput = document.querySelector('.js-message-input');
 
 function menuTogglerOffice() {
   office.classList.toggle('js-menu-open');
@@ -30,6 +31,12 @@ function openPopup() {
   document.body.style.overflow = 'hidden';
   var scrollWidth = document.body.clientWidth - previousBodyWidth;
   document.body.style.marginRight = document.body.clientWidth + scrollWidth;
+
+  document.onkeydown = function(e) {
+    if (e.key === 'Escape') {
+      closePopup();
+    }
+  };
 }
 
 function closePopup() {
@@ -37,26 +44,59 @@ function closePopup() {
   document.body.style.overflow = 'scroll';
 }
 
-function validation() {
+// function validation(value) {
 
-}
+// }
 
-function setMask() {
-  phoneInput.setAttribute('value', '+7');
-}
+// function setMask(value) {
+
+//   function createMask(value) {
+//     var matrix = '+7 (___)_______';
+//     var i = 0;
+
+//     // возьми все не цифры и замени пустым местом. вернется 7
+//     var def = matrix.replace(/\D/g, '');
+//     var val = value.replace(/\D/g, '');
+
+//     if (def.length >= val.length) {
+//       val = def;
+//     }
+
+//     value = matrix.replace(/./g, function (a) {
+//       if (/[_\d]/.test(a) && i < val.length) {
+//         val.charAt(i++);
+
+//         if (i >= val.length) {
+//           return '';
+//         }
+//         return a;
+//       }
+//       // return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? '' : a;
+//     });
+//   }
+
+// }
 
 function onSubmitForm(e) {
   e.preventDefault();
-  var userPhone = phoneInput.nodeValue;
+  var userPhone = phoneInput.value;
+  var userName = nameInput.value;
+  var userMessage = messageInput.value;
+  // validation(userPhone);
+
   localStorage.setItem('phone', userPhone);
+  localStorage.setItem('name', userName);
+  localStorage.setItem('message', userMessage);
+
 }
 
+form.addEventListener('submit', onSubmitForm);
 openPopupButton.addEventListener('click', openPopup);
 closePopupButton.addEventListener('click', closePopup);
 pages.addEventListener('click', menuTogglerPages);
 office.addEventListener('click', menuTogglerOffice);
-phoneInput.addEventListener('change', validation);
-phoneInput.addEventListener('click', setMask);
+// phoneInput.addEventListener('change', setMask(phoneInput.target.value));
+// phoneInput.addEventListener('click', setMask);
 overlay.addEventListener('click', closePopup);
-form.addEventListener('submit', onSubmitForm);
+
 
